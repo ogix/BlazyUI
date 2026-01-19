37698ee7ec385e176a7ce3d3e34e8826890f34ff
Olegas <ogix777@gmail.com>
2026-01-19 12:25:34 +0200

    feat(modal): add internal ModalReference for tracking active modals

---
+++ src/BlazyUI/Components/Modal/ModalReference.cs
@@ -0,0 +1,18 @@
+// src/BlazyUI/Components/Modal/ModalReference.cs
+namespace BlazyUI;
+
+internal class ModalReference
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public ModalType Type { get; init; }
+    public string? Title { get; init; }
+    public string? Message { get; init; }
+    public string OkText { get; init; } = "OK";
+    public string CancelText { get; init; } = "Cancel";
+    public bool CloseOnBackdropClick { get; init; }
+    public Type? ComponentType { get; init; }
+    public Dictionary<string, object>? Parameters { get; init; }
+    public string? CssClass { get; init; }
+    public TaskCompletionSource<ModalResult> TaskCompletionSource { get; init; } = default!;
+    public ModalInstance? Instance { get; set; }
+}
